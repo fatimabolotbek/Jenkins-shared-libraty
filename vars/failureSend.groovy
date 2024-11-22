72e38a5517f81e62d66dc67eaa59f2753f73ee0c
fatimabolotbek <zhumalieva91@gmail.com>
2024-11-21 19:53:27 -0600

    test

--- vars/failureSend.groovy
+++ vars/failureSend.groovy
@@ -1,5 +1,5 @@
 def call(){
-        emailext (subject: "FAILED: Job  ${env.JOB_NAME}", 
-                     body: "Job ${env.JOB_NAME}  with B ${env.BUILD_NUMBER} has finished with failure.. This is the job: ${env.JOB_URL}", 
-                     to: "bolotbekfatima1@gmail.com")
+        emailext (subject: "SUCCESSFUL: Job  ${env.JOB_NAME}", 
+                    body: "Job ${env.JOB_NAME}  with B ${env.BUILD_NUMBER} has finished with failure.. This is the job: ${env.JOB_URL}", 
+                    to: "bolotbekfatima1@gmail.com")
 }
